a612c8b45df12ee6b419550f5ec15e22afd41f92
Sreerama Pavani <sreeramakumar.pavani@igt.com>
2016-04-27 16:23:40 -0400

    added first feature branch

---
+++ src/js/firstfeature.js
@@ -0,0 +1,19 @@
+/**
+ * Created by spavani on 4/27/2016.
+ */
+
+class Person{
+
+    constructor(firstname,lastname){
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    get firstName(){
+        return this.firstName;
+    }
+
+    get lastName(){
+        this.lastName;
+    }
+}
